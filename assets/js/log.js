9f61d6abf758738f5fe755bfccba68db435c2858
Kohei Yoshida <was914hs@gmail.com>
2026-04-26 00:10:42 +0900

    仕様を変更しJSONからfetchでhtmlに流し込む方針にした

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,52 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const activityLogs = [
-
-        {
-            date: "2026.03.24",
-            type: "feat",
-            msg: "画像を圧縮し再アップロードした"
-        },
-
-        {
-            date: "2026.03.23",
-            type: "feat",
-            msg: "ABOUTセクションのスキル構成をアップデート"
-        },
-
-        {
-            date: "2026.03.20",
-            type: "fix",
-            msg: "コンタクトフォームのバリデーションを修正"
-        },
-
-        {
-            date: "2026.03.15",
-            type: "work",
-            msg: "新作MV『グッバイ宣言』をWORKSに追加"
-        },
-
-        {
-            date: "2026.02.26",
-            type: "sys",
-            msg: "公式サイトをNext.jsへ移行開始"
-        }
-
-    ];
-
-    // ここを小文字の logContainer で統一
     const logContainer = document.getElementById('log-container');
 
     if (logContainer) {
-        activityLogs.forEach(log => {
-            const logItem = document.createElement('div');
-            logItem.className = 'log-item';
-            logItem.innerHTML = `
-                <span class="log-date">${log.date}</span>
-                <span class="log-type type-${log.type}">${log.type}</span>
-                <span class="log-msg">${log.msg}</span>
-            `;
-            logContainer.appendChild(logItem);
-        });
+        //外部のJSONファイルを読みに行く
+        fetch('./assets/data/log.json')
+            .then(response => response.json())
+            .then(activityLogs => {
+
+                // latestLogsを切り出す
+                const latestLogs = activityLogs.slice(0, 4);
+
+                // 最初の[0 ~ 4]を切り出す
+                latestLogs.forEach(log => {
+
+                    // divにデータを放り込む
+                    const logItem = document.createElement('div');
+
+                    // データの送り先は <div class="log-item">
+                    logItem.className = 'log-item';
+                    logItem.innerHTML = `
+                        <span class="log-date">${log.date}</span>
+                        <span class="log-type type-feat type-fix type-work type-sys${log.type}">${log.type}</span>
+                        <span class="log-msg">${log.msg}</span>
+                    `;
+                    logContainer.appendChild(logItem);
+                });
+            })
+            .catch(error => console.log("ログが読めませんでした", error));
     }
 });
